b354a0cf2b08d66fee7ad96936cdae012c56e2a1
Mhamed Beghdadi <mhameds@gmail.com>
2016-11-25 17:05:48 +0100

    commit add Course.cs

---
+++ LMS System/Models/Course.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS_System.Models
+{
+    public class Course
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+
+
+        public virtual ICollection<ApplicationUser> Students { get; set; }
+        public virtual ICollection<ApplicationUser> Teachers { get; set; } 
+        public virtual ICollection<Module> Modules { get; set; }
+        public virtual ICollection<Document> CourseDocuments { get; set; }
+    }
+}
