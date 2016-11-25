bca08c8e4a89535265c8748195dbaf444976591a
andla007 <andla@tc.se>
2016-11-25 16:21:58 +0100

    First Commit

---
+++ LMS System/Models/LMSModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS_System.Models
+{
+    public class LMSModel
+    {
+        public int Id { get; set; }
+        //This is a test
+    }
+}
---
+++ LMS System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LMS_System.Startup))]
+namespace LMS_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
