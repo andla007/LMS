b597b6a922ce6e48871e3a55dddb1a6cf10acdae
andla007 <andla@tc.se>
2016-11-28 16:17:12 +0100

    Added modules

--- LMS System/Models/IdentityModels.cs
+++ LMS System/Models/IdentityModels.cs
@@ -37,6 +37,7 @@ namespace LMS_System.Models
         }
 
         public System.Data.Entity.DbSet<LMS_System.Models.Course> Courses { get; set; }
+
         public System.Data.Entity.DbSet<LMS_System.Models.Module> Modules { get; set; }
     }
 }
