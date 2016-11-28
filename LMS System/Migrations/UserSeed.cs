8b901be9bc9ebefd0a3feb202db7f39e08ab7615
andla007 <andla@tc.se>
2016-11-28 10:49:18 +0100

    Added user seed and changed password requirement to much easier handling. Remember increase restriction after full verison launch

---
+++ LMS System/Migrations/UserSeed.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS_System.Migrations
+{
+    public class UserSeed
+    {
+        public string EMail { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get { return FirstName + " " + LastName; } }
+        public System.DateTime TimeOfRegistration { get; set; }
+    }
+}
