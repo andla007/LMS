a125d31b5aef585ce1e40e6d522a2a703ec60270
andla007 <andla@tc.se>
2016-11-28 11:02:13 +0100

    Added a user seed class

--- LMS System/Migrations/UserSeed.cs
+++ LMS System/Migrations/UserSeed.cs
@@ -7,7 +7,7 @@ namespace LMS_System.Migrations
 {
     public class UserSeed
     {
-        public string EMail { get; set; }
+        public string EMail { get; set; }//
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string FullName { get { return FirstName + " " + LastName; } }
